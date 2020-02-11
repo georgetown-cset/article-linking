3131a8167df8b2abc8257fc63037b3c18bd33ed0
Jennifer Melot <jtmelot@gmail.com>
2020-02-11 10:24:16 -0500

    tablename and syntax bugfixes

--- sql_approach/sql/pairwise_doi_match_template.sql
+++ sql_approach/sql/pairwise_doi_match_template.sql
@@ -1,2 +1,2 @@
 select a.id as {TABLE1}_id, w.id as {TABLE2}_id
-from {DATASET}.{TABLE1}_dois a inner join {DATASET}.{TABLE2}_dois w on a.clean_doi = w.clean_doi
+from {DATASET}.{TABLE1}_unambiguous_dois a inner join {DATASET}.{TABLE2}_unambiguous_dois w on a.clean_doi = w.clean_doi
