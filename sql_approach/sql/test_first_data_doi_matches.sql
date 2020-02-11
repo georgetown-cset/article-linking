239b4dcec07836b61b79ed453179e80b1fb9cfc8
Jennifer Melot <jtmelot@gmail.com>
2020-02-11 17:19:13 -0500

    reporting on "first" method

---
+++ sql_approach/sql/test_first_data_doi_matches.sql
@@ -0,0 +1,3 @@
+select {TABLE1}_id, {TABLE2}_id from {DATASET}.first_4_plus_3_plus_2_plus_1
+where ({TABLE1}_id is not null) and ({TABLE2}_id is not null) and
+  ({TABLE1}_id in (select {TABLE1}_id from {DATASET}.{TABLE1}_{TABLE2}_dois))
