8506ccf154865b3fc539942b8ee6094ca3ee3e83
Jennifer Melot <jtmelot@gmail.com>
2020-04-18 21:40:46 -0400

    updating lid beam pipeline

--- utils/run_lid.py
+++ utils/run_lid.py
@@ -4,7 +4,6 @@
 import argparse
 import ast
 import chardet
-import langdetect
 import json
 import logging
 import pycld2 as cld2
@@ -18,16 +17,12 @@ class LangId(beam.DoFn):
     def __init__(self, fields_to_lid: list) -> None:
         self.fields_to_lid = fields_to_lid
 
-    def run_langdetect(self, record: dict):
-        for field in self.fields_to_lid:
-            if field in record:
-                try:
-                    record[field + "_langdetect_result"] = langdetect.detect(record[field])
-                except langdetect.lang_detect_exception.LangDetectException as e:
-                    logging.warning(e)
-        return record
-
-    def run_cld2(self, record: dict):
+    def add_cld2_outputs(self, record: dict) -> None:
+        '''
+        Adds cld2 translations and confidence measures to `record`
+        :param record: Record containing fields we want to run LID on. Mutated by this method.
+        :return: None. Mutates `record`
+        '''
         for field in self.fields_to_lid:
             # ... the default success state
             record[field + "_cld2_lid_success"] = False
@@ -54,11 +49,10 @@ class LangId(beam.DoFn):
                 logging.warning(e)
             except UnicodeDecodeError as e:
                 logging.warning(e)
-        return record
 
     def process(self, record_str: str) -> iter:
         record = ast.literal_eval(record_str)
-        self.run_langdetect(record)
+        self.add_cld2_outputs(record)
         yield json.dumps(record)
 
 def run_pipeline(input_dir: str, output_dir: str, fields_to_lid: list, pipeline_args: list) -> None:
@@ -66,22 +60,22 @@ def run_pipeline(input_dir: str, output_dir: str, fields_to_lid: list, pipeline_
     Run a beam pipeline that cleans all records within all files in input_dir
     :param input_dir: Directory of jsonl files to run LID on. Can be local or gcs
     :param output_dir: Directory where post-LID files should be written. Can be local or gcs
-    :param fields_to_lid: Fields to clean within each record
+    :param fields_to_lid: Fields to run lid on within each record
     :param pipeline_args: Beam pipeline args
     :return: None
     '''
     with beam.Pipeline(options=PipelineOptions(pipeline_args)) as p:
         (p | "Read from Text" >> beam.io.ReadFromText(input_dir)
-            | "Scrub Text" >> beam.ParDo(LangId(fields_to_lid))
+            | "Run LID" >> beam.ParDo(LangId(fields_to_lid))
             | "Write to Text" >> beam.io.WriteToText(output_dir))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_dir")
-    parser.add_argument("output_dir")
-    parser.add_argument("fields_to_clean",
-                        help="comma-separated list of fields that should be cleaned within each record")
+    parser.add_argument("--input_dir", required=True)
+    parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--fields_to_translate", required=True,
+                        help="comma-separated list of fields that should be translated")
     args, pipeline_args = parser.parse_known_args()
 
     run_pipeline(args.input_dir, args.output_dir, args.fields_to_clean.split(","), pipeline_args)
