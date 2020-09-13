9285fe8526c547331a0915561e5e12f3eabe1497
Jennifer Melot <jtmelot@gmail.com>
2020-09-13 15:17:18 -0400

    updating to handle null normalization

--- sql/ds_authors.sql
+++ sql/ds_authors.sql
@@ -1,5 +1,5 @@
 -- get digital science author last names
 select
   id,
-  ARRAY(select last_name from UNNEST(author_affiliations)) as last_names
+  ARRAY(select last_name from UNNEST(author_affiliations) where last_name is not null) as last_names
 from gcp_cset_digital_science.dimensions_publications_latest
