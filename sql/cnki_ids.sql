14351a31324fe2645c60e65af0e90e2ed7456180
Jennifer Melot <jtmelot@gmail.com>
2020-10-20 10:21:09 -0400

    updating cnki ids query

--- sql/cnki_ids.sql
+++ sql/cnki_ids.sql
@@ -1,2 +1,10 @@
--- get cnki ids (used in validation)
-select distinct(cnki_document_id) as id from gcp_cset_cnki.cset_cnki_journals_id_mappings
+-- get cnki ids (used in validation). Formerly, we got these from gcp_cset_cnki.cset_cnki_journals_id_mappings,
+-- but per conversation with Daniel, not all these ids will appear in any of the three metadata tables (below)
+-- we use to construct CNKI articles. So instead, we'll union the ids that appear in the metadata tables we use.
+select distinct(id) from (
+  select id from {{params.dataset}}.cnki_year_doi_authors
+  union all
+  select id from {{params.dataset}}.cnki_title
+  union all
+  select id from {{params.dataset}}.cnki_abstract
+)
