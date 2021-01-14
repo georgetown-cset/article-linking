d2137dafcadaedaced21c262c71be79649569442
Jennifer Melot <jtmelot@gmail.com>
2021-01-13 23:25:23 -0500

    explictly name year column

--- sql/union_metadata.sql
+++ sql/union_metadata.sql
@@ -1,24 +1,24 @@
 -- glue all the metadata together into one table
-select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64), last_names, null as references,
-  "arxiv" as dataset
+select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64) as year, last_names,
+  null as references, "arxiv" as dataset
   from {{params.dataset}}.arxiv_metadata
 UNION ALL
-select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64), last_names, references,
-  "wos" as dataset
+select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64) as year, last_names,
+  references, "wos" as dataset
   from {{params.dataset}}.wos_metadata
 UNION ALL
-select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64), last_names, references,
-  "ds" as dataset
+select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64) as year, last_names,
+  references, "ds" as dataset
   from {{params.dataset}}.ds_metadata
 UNION ALL
-select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64), last_names, references,
-  "mag" as dataset
+select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64) as year, last_names,
+  references, "mag" as dataset
 from {{params.dataset}}.mag_metadata
 UNION ALL
-select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64), last_names, null as references,
-  "cnki" as dataset
+select cast(id as string) as id, title, abstract, clean_doi, cast(year as int64) as year, last_names,
+  null as references, "cnki" as dataset
 from {{params.dataset}}.cnki_metadata
 UNION ALL
-select cast(id as string) as id, title, abstract, null as clean_doi, cast(year as int64), last_names, null as references,
-  "pwc" as dataset
+select cast(id as string) as id, title, abstract, null as clean_doi, cast(year as int64) as year, last_names,
+  null as references, "pwc" as dataset
 from {{params.dataset}}.papers_with_code_metadata
