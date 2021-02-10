9c63f51c793a0a653c2b6a6996cc0a99fbd23fc3
Jennifer Melot <jtmelot@gmail.com>
2021-02-10 15:14:43 -0500

    using min doi in merged meta table

--- sql/article_merged_meta.sql
+++ sql/article_merged_meta.sql
@@ -3,7 +3,7 @@ select
   merged_id,
   max(title) as title,
   max(abstract) as abstract,
-  max(clean_doi) as doi,
+  min(clean_doi) as doi,
   max(year) as year,
   split(max(last_names_norm), " ") as normalized_last_names
 from {{params.dataset}}.article_links_with_meta group by merged_id
