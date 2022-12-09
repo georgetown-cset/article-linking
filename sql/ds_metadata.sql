c9aaea43e3f76e1c590055a276fc70f4b000e68f
Jennifer Melot <jtmelot@gmail.com>
2022-12-09 10:46:55 -0500

    Match on both doi variants for DS
    Part of #8

--- sql/ds_metadata.sql
+++ sql/ds_metadata.sql
@@ -13,3 +13,18 @@ INNER JOIN
   {{staging_dataset}}.ds_authors a
 ON
   d.id = a.id
+UNION ALL
+SELECT
+  d.id,
+  d.title,
+  d.abstract,
+  lower(d.resulting_publication_doi) as clean_doi,
+  d.year,
+  a.last_names,
+  array_to_string(array(select r from unnest(d.references) as r order by r), ",") as references
+FROM
+  gcp_cset_digital_science.dimensions_publications_with_abstracts_latest d
+INNER JOIN
+  {{staging_dataset}}.ds_authors a
+ON
+  d.id = a.id
