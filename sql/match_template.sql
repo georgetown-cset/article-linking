7fe58f332161da86bc54a7bedfe576ca5bdf4267
Jennifer Melot <jtmelot@gmail.com>
2023-03-23 17:00:01 -0400

    Add empty string check except for year

--- sql/match_template.sql
+++ sql/match_template.sql
@@ -4,5 +4,5 @@ from {{staging_dataset}}.all_metadata_norm_filt as a
 inner join
 {{staging_dataset}}.all_metadata_norm_filt as m
 on (a.{{ params.strong }} = m.{{ params.strong }}) and
-    (a.{{ params.strong }} is not null) and
-    (a.{{ params.other }} = m.{{ params.other }}) and (a.{{ params.other }} is not null)
+    (a.{{ params.strong }} is not null) and (a.{{ params.strong }} != "") and
+    (a.{{ params.other }} = m.{{ params.other }}) and (a.{{ params.other }} is not null) {{params.additional_checks}}
