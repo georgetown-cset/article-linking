2f2dbb6303c6f18b4ab5c393b27880f256d9d89c
Jennifer Melot <jtmelot@gmail.com>
2023-04-20 14:04:07 -0400

    Revert testing changes

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,13 +1,13 @@
-gsutil cp linkage_dag_v3.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
+gsutil cp linkage_dag.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
 gsutil cp scholarly_lit_trigger.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
-gsutil -m cp sql/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/article_linkage_v3/
-gsutil -m cp sequences/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sequences/article_linkage_v3/
-gsutil cp schemas/* gs://airflow-data-exchange/article_linkage_v3/schemas/
-gsutil -m cp schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/article_linkage_v3/
-gsutil cp utils/create_merge_ids.py gs://airflow-data-exchange/article_linkage_v3/vm_scripts/
-gsutil cp utils/run_simhash.py gs://airflow-data-exchange/article_linkage_v3/vm_scripts/
-gsutil cp utils/my_simhash.py gs://airflow-data-exchange/article_linkage_v3/vm_scripts/
-gsutil cp utils/article_linkage_lid_dataflow_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_v3_lid_dataflow_requirements.txt
-gsutil cp utils/article_linkage_text_clean_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_v3_text_clean_requirements.txt
-gsutil cp utils/clean_corpus.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts_v3/
-gsutil cp utils/run_lid.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts_v3/
+gsutil -m cp sql/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/article_linkage/
+gsutil -m cp sequences/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sequences/article_linkage/
+gsutil cp schemas/* gs://airflow-data-exchange/article_linkage/schemas/
+gsutil -m cp schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/article_linkage/
+gsutil cp utils/create_merge_ids.py gs://airflow-data-exchange/article_linkage/vm_scripts/
+gsutil cp utils/run_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
+gsutil cp utils/my_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
+gsutil cp utils/article_linkage_lid_dataflow_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_lid_dataflow_requirements.txt
+gsutil cp utils/article_linkage_text_clean_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_text_clean_requirements.txt
+gsutil cp utils/clean_corpus.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts/
+gsutil cp utils/run_lid.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts/
--- sql/simhash_input.sql
+++ sql/simhash_input.sql
@@ -11,7 +11,7 @@ where
     id not in (
       select a.id from {{staging_dataset}}.all_metadata_norm_filt a
       left join
-      gcp_cset_links_v2.all_metadata_with_cld2_lid b
+      {{production_dataset}}.all_metadata_with_cld2_lid b
       on a.id = b.id
       where (a.title = b.title) and (a.abstract = b.abstract) and (a.year = b.year) and (a.title != "") and
       (a.title is not null) and (a.abstract != "") and (a.abstract is not null) and (a.year is not null)
