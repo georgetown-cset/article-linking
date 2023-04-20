770a0955fc1872505fe0037db666735458927ea8
Jennifer Melot <jtmelot@gmail.com>
2023-04-20 13:48:51 -0400

    Revert more testing changes

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -6,8 +6,9 @@ gsutil cp schemas/* gs://airflow-data-exchange/article_linkage/schemas/
 gsutil -m cp schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/article_linkage/
 gsutil cp utils/create_merge_ids.py gs://airflow-data-exchange/article_linkage/vm_scripts/
 gsutil cp utils/run_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
+gsutil cp utils/run_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
 gsutil cp utils/my_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
-gsutil cp utils/article_linkage_lid_dataflow_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_lid_dataflow_requirements.txt
-gsutil cp utils/article_linkage_text_clean_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/article_linkage_text_clean_requirements.txt
+gsutil cp utils/article_linkage_lid_dataflow_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/
+gsutil cp utils/article_linkage_text_clean_requirements.txt gs://us-east1-production2023-cc1-01d75926-bucket/dags/requirements/
 gsutil cp utils/clean_corpus.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts/
 gsutil cp utils/run_lid.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/linkage_scripts/
