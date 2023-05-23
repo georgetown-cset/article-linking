3ec7df17d0188988ff8047d9989146f9517a4af8
Jennifer Melot <jtmelot@gmail.com>
2023-05-23 12:01:40 -0400

    Remove contents of directories before populating from local files

--- push_to_airflow.sh
+++ push_to_airflow.sh
@@ -1,8 +1,12 @@
 gsutil cp linkage_dag.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
 gsutil cp scholarly_lit_trigger.py gs://us-east1-production2023-cc1-01d75926-bucket/dags/
+gsutil rm -r gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/article_linkage/*
 gsutil -m cp sql/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sql/article_linkage/
+gsutil rm -r gs://us-east1-production2023-cc1-01d75926-bucket/dags/sequences/article_linkage/*
 gsutil -m cp sequences/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/sequences/article_linkage/
+gsutil rm -r gs://airflow-data-exchange/article_linkage/schemas/*
 gsutil cp schemas/* gs://airflow-data-exchange/article_linkage/schemas/
+gsutil rm -r gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/article_linkage/*
 gsutil -m cp schemas/* gs://us-east1-production2023-cc1-01d75926-bucket/dags/schemas/article_linkage/
 gsutil cp utils/create_merge_ids.py gs://airflow-data-exchange/article_linkage/vm_scripts/
 gsutil cp utils/run_simhash.py gs://airflow-data-exchange/article_linkage/vm_scripts/
