404de2f60022479b2f6bc611f0713cc9682c4343
Jennifer Melot <jtmelot@gmail.com>
2023-05-23 12:39:46 -0400

    Left not cross join

--- sql/s2_ids.sql
+++ sql/s2_ids.sql
@@ -3,5 +3,5 @@ SELECT
   DISTINCT(CAST(corpusid AS string)) as id
 FROM
   semantic_scholar.papers
-CROSS JOIN unnest(publicationtypes) as publication_type
+LEFT JOIN unnest(publicationtypes) as publication_type
 WHERE (publication_type IS NULL) OR (NOT (publication_type IN ("Dataset", "Editorial", "LettersAndComments", "News", "Review")))
