5a6bcc05f5432f845dc5b9e85e207b283d92795a
Jennifer Melot <jtmelot@gmail.com>
2023-07-19 11:59:59 -0400

    Lint query and add better docs

--- sql/mapped_references.sql
+++ sql/mapped_references.sql
@@ -1,11 +1,32 @@
--- map references to their carticle ids
-select distinct links1.merged_id as id, links2.merged_id as ref_id from
-(select id, reference from {{ staging_dataset }}.all_metadata_with_cld2_lid cross join unnest(split(references, ",")) as reference
-  where reference in (
-    select orig_id from {{ staging_dataset }}.article_links_with_dataset
-)) as references
-left join {{ staging_dataset }}.article_links as links1
-on references.id = links1.orig_id
-left join {{ staging_dataset }}.article_links as links2
-on references.reference = links2.orig_id
-where (links1.merged_id is not null) and (links2.merged_id is not null)
+-- Create a table giving each publication's references, identified by their merged_ids.
+-- The all_metadata_with_cld2_lid table contains references identified by vendor IDs for each orig_id.
+-- For each merged_id, we take all its orig_ids' references, and look up the merged_ids of the references.
+-- We exclude references that appear outside our merged corpus.
+SELECT
+  DISTINCT links1.merged_id AS id,
+  links2.merged_id AS ref_id
+FROM (
+  SELECT
+    id,
+    reference
+  FROM
+    {{ staging_dataset }}.all_metadata_with_cld2_lid
+  CROSS JOIN
+    UNNEST(SPLIT(references, ",")) AS reference
+  WHERE
+    reference IN (
+    SELECT
+      orig_id
+    FROM
+      {{ staging_dataset }}.article_links_with_dataset )) AS references
+LEFT JOIN
+  {{ staging_dataset }}.article_links AS links1
+ON
+references.id = links1.orig_id
+LEFT JOIN
+  {{ staging_dataset }}.article_links AS links2
+ON
+references.reference = links2.orig_id
+WHERE
+  (links1.merged_id IS NOT NULL)
+  AND (links2.merged_id IS NOT NULL)
