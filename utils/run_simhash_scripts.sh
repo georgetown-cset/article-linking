ec804ef3ee94aec720d4a48f2cec6e372a57a367
Jennifer Melot <jtmelot@gmail.com>
2024-01-23 11:15:30 -0500

    Ensure done files cleared out in case of retry

--- utils/run_simhash_scripts.sh
+++ utils/run_simhash_scripts.sh
@@ -1,4 +1,5 @@
 cd /mnt/disks/data/run
+gsutil rm gs://airflow-data-exchange/article_linkage/tmp/done_files/simhash_is_done
 python3 run_simhash.py simhash_input simhash_results --simhash_indexes simhash_indexes --new_simhash_indexes new_simhash_indexes
 cp -r article_pairs usable_ids
 cp simhash_results/* article_pairs/
